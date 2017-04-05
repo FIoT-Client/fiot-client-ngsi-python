0101df5411e79891bbc34ca05faf900e064c1e8d
Lucas Cristiano Calixto Dantas <lucascristiano27@gmail.com>
2017-04-05 18:46:59 -0300

    Add Elasticsearch 0.9 sink

--- config/sth/config.js
+++ config/sth/config.js
@@ -64,7 +64,7 @@ config.database = {
   password: '',
   // The URI to use for the database connection. It supports replica set URIs. This does not
   // include the "mongo://" protocol part. Default value: "localhost:27017"
-  URI: 'mongodb-comet:27017',
+  URI: 'mongodb-cygnus:27017',
   // The name of the replica set to connect to, if any. Default value: "".
   replicaSet: '',
   // The prefix to be added to the service for the creation of the databases. Default value: "sth".
