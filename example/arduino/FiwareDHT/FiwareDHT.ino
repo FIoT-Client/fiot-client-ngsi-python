0853f15d1c1b35ae9a780edf9736b89e268932f6
Lucas Cristiano <lucascristiano27@gmail.com>
2017-08-09 08:38:34 -0300

    Add command execution on Arduino

--- example/arduino/FiwareDHT/FiwareDHT.ino
+++ example/arduino/FiwareDHT/FiwareDHT.ino
@@ -5,7 +5,7 @@
 
 // Update these with values suitable for your network.
 byte mac[] = { 0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
-IPAddress server(10, 7, 49, 20);
+IPAddress server(10, 7, 49, 163);
 
 /**
 //Uncomment if you want to set a manual IP
@@ -21,10 +21,15 @@ PubSubClient client(ethClient);
 dht DHT;
 
 #define DHT21_PIN 2
+#define LED_PIN   3
+
+char* cmdAnswerTopic = "/4jggokgpepnvsb2uv4s40d59ov1/STELA_LED/cmdexe"; // /API_KEY/DEVICE_ID/cmdexe + 5
 
 void setup() {
   Serial.begin(115200);
   delay(2000);
+
+  pinMode(LED_PIN, OUTPUT);
 
   client.setServer(server, 1883);
   
@@ -38,6 +43,7 @@ void publishMeasurements() {
   char strHumidity[6];
   char strTemperature[6];
 
+  char measurementPayloadBuf[40];
   char measurementPayloadBuf[40];
 
   int chk = DHT.read21(DHT21_PIN);
@@ -58,6 +64,28 @@ void publishMeasurements() {
       delay(2000);
       break;
   }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+	Serial.print("Message arrived [");
+	Serial.print(topic);
+	Serial.println("] ");
+
+	//for (int i=0; i < length; i++) {
+	//	Serial.print((char)payload[i]);
+	//}
+
+	if (cmd.strcmp((char *) payload, "ON")) {
+		digitalWrite(LED_PIN, HIGH);
+		client.publish(cmdAnswerTopic, "change_state|Done");
+	} else if (cmd.strcmp((char *) payload, "OFF")) {
+		digitalWrite(LED_PIN, LOW);
+		client.publish(cmdAnswerTopic, "change_state|Done");
+	} else {
+		client.publish(cmdAnswerTopic, "change_state|Error");
+	}
+
+	Serial.println("Answer sent");
 }
 
 void reconnect() {
