8e491e989259555d6384f360ca53426628971fe6
Lucas Cristiano <lucascristiano27@gmail.com>
2020-11-07 01:48:47 -0300

    Adds empty values when config value not provided

--- fiotclient/config.py
+++ fiotclient/config.py
@@ -3,40 +3,41 @@ class FiwareConfig:
 
     # TODO Check and notify mandatory parameters on input config dict
 
-    def __init__(self, config_json: dict):
-        self.service = config_json['fiwareService']
-        self.service_path = config_json['fiwareServicePath']
+    def __init__(self):
+        self.__init__({})
 
-        self.cb_host = config_json['contextBroker']['host']
-        self.cb_port = config_json['contextBroker']['port']
+    def __init__(self, config_json: dict):
+        self.service = config_json.get('fiwareService', '')
+        self.service_path = config_json.get('fiwareServicePath', '')
+
+        self.cb_host = config_json.get('contextBroker', {}).get('host', '')
+        self.cb_port = config_json.get('contextBroker', {}).get('port', '')
         # TODO Include OAuth param
 
-        self.iota_aaa = config_json['iota']['oauth']
-        self.iota_host = config_json['iota']['host']
-        self.iota_north_port = config_json['iota']['northPort']
-        self.iota_protocol_port = config_json['iota']['protocolPort']
-        self.api_key = config_json['iota']['apiKey']
+        self.iota_aaa = config_json.get('iota', {}).get('oauth', '')
+        self.iota_host = config_json.get('iota', {}).get('host', '')
+        self.iota_north_port = config_json.get('iota', {}).get('northPort', '')
+        self.iota_protocol_port = config_json.get('iota', {}).get('protocolPort', '')
+        self.api_key = config_json.get('iota', {}).get('apiKey', '')
 
-        self.mqtt_broker_host = config_json['mqttBroker']['host']
-        self.mqtt_broker_port = config_json['mqttBroker']['port']
+        self.mqtt_broker_host = config_json.get('mqttBroker', {}).get('host', '')
+        self.mqtt_broker_port = config_json.get('mqttBroker', {}).get('port', '')
 
-        self.sth_host = config_json['sthComet']['host']
-        self.sth_port = config_json['sthComet']['port']
+        self.sth_host = config_json.get('sthComet', {}).get('host', '')
+        self.sth_port = config_json.get('sthComet', {}).get('port', '')
 
-        self.cygnus_host = config_json['cygnus']['host']
-        self.cygnus_port = config_json['cygnus']['port']
-        self.cygnus_notification_host = config_json['cygnus']['notificationHost']
+        self.cygnus_host = config_json.get('cygnus', {}).get('host', '')
+        self.cygnus_port = config_json.get('cygnus', {}).get('port', '')
+        self.cygnus_notification_host = config_json.get('cygnus', {}).get('notificationHost', '')
 
-        self.perseo_host = config_json['perseo']['host']
-        self.perseo_port = config_json['perseo']['port']
+        self.perseo_host = config_json.get('perseo', {}).get('host', '')
+        self.perseo_port = config_json.get('perseo', {}).get('port', '')
 
-        if config_json['iota']['aaa']:
-            self.token = config_json['user']['token']
+        if config_json.get('iota', {}).get('aaa', ''):
+            self.token = config_json.get('user', {}).get('token', '')
             self.token_show = self.token[1:5] + "*" * 70 + self.token[-5:]
         else:
             self.token = 'NULL'
             self.token_show = 'NULL'
 
-        self.host_id = config_json['local']['hostId']
-
-        self.fiware_service_path = config_json['fiwareServicePath']
+        self.host_id = config_json.get('local', {}).get('hostId', '')
