1432005567e1f599f00932b0a453f091b53be113
Lucas Cristiano <lucascristiano27@gmail.com>
2021-09-06 20:56:51 -0300

    Adds version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ README = os.path.join(os.path.dirname(__file__), 'README.md')
 
 setup(
     name='fiotclient',
-    version='0.7.14',
+    version='0.7.16',
     packages=find_packages(),
     include_package_data=True,
     license='MIT',
